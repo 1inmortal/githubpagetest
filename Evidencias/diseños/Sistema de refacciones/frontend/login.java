fb8054c828ffa2af42d7af0663960c96225ab6ce
1inmortal <jarmando2965@gmail.com>
2025-01-10 20:44:54 -0600

    2.3.7 - Cyberponk-2077-UI

---
+++ Evidencias/diseños/Sistema de refacciones/frontend/login.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+HashMap<String, String> users = new HashMap<>();
+users.put(key:"admin", "admin");
