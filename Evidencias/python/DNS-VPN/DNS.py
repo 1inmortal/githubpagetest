5d990343e6b58f2bb90e5a985bf9adfaa0b56cb1
1inmortal <jarmando2965@gmail.com>
2025-03-06 07:26:28 -0600

    2.7.4

--- Evidencias/python/DNS-VPN/DNS.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def limpiar_dns_windows():
-    # Limpiar la caché DNS en Windows
-    os.system('ipconfig /flushdns')
-    print("Caché DNS limpiada en Windows.")
-
-# Llamar a la función
-limpiar_dns_windows()
--- Evidencias/python/scraper/config.py
+++
@@ -1,3 +0,0 @@
-# Configuración del rastreador
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-TIMEOUT = 5
