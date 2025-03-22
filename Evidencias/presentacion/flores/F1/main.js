6d9df4dd168218a09c17c42a6153f0cfbde2a1f4
1inmortal <jarmando2965@gmail.com>
2025-03-22 17:18:05 -0500

    2.8.6

---
+++ Evidencias/presentacion/flores/F1/main.js
@@ -0,0 +1,10 @@
+// main.js
+onload = () => {
+  document.body.classList.remove("container");
+
+  const audio = document.querySelector('audio');
+  // Necesitamos una interacción del usuario para reproducir el audio
+  document.addEventListener('click', () => {
+    audio.play();
+  });
+};
