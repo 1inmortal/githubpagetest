3b387323c9167d7e9d7585d858bc06b818526f7a
INMORTAL <jarmando2965@gmail.com>
2025-10-16 08:59:22 -0500

    zonagrafica: arregla rutas de assets (espacios y acentos), normaliza asset(), sirve img via publicDir y alinea BASE_URL con Vite

--- public/webs/zonagrafica/vite.config.js
+++ public/webs/zonagrafica/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/githubpagetest/public/webs/zonagrafica/dist/',
   root: '.',
+  publicDir: 'img',
   server: {
     port: 3001,
     open: '/index.html',
