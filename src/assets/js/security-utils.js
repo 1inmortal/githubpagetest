b970b6f2565b79f2afdc11d1ed3ddefd01c806ea
INMORTAL <jarmando2965@gmail.com>
2026-01-20 20:24:08 -0600

    chore: Agregar integridad SRI a los scripts de GSAP, Anime.js y otros recursos externos para mejorar la seguridad y prevenir ataques de inyección. Actualizar las rutas de los scripts en múltiples archivos HTML.

--- src/assets/js/security-utils.js
+++ src/assets/js/security-utils.js
@@ -91,7 +91,8 @@ class ContentSanitizer {
         this.domPurify.setConfig({
             ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a', 'p', 'br', 'span', 'div'],
             ALLOWED_ATTR: ['href', 'target', 'rel', 'class', 'id'],
-                            ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp):|[^a-z]|[a-z+.-]+(?:[^a-z+.-:]|$))/i,
+            // Regex corregido: guion al final para evitar interpretación como rango (CWE-20)
+            ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|tel|callto|cid|xmpp):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
             FORBID_TAGS: ['script', 'style', 'iframe', 'object', 'embed', 'form', 'input'],
             FORBID_ATTR: ['onerror', 'onload', 'onclick', 'onmouseover', 'onfocus', 'onblur']
         });
