1fbd99cacbf9a8ddc99719a2ff68fd849fcd0849
INMORTAL <jarmando2965@gmail.com>
2026-01-21 19:41:57 -0600

    Corregir rutas a relativas en index.html y vite.config.js de cenaduria

--- public/webs/cenaduria/react-app/vite.config.js
+++ public/webs/cenaduria/react-app/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/githubpagetest/public/webs/cenaduria/react-app/dist/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
